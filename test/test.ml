ae96d83004cd856ac28a521ddb0ceed738bafa9c
Vincent Bernardoff <vb@luminar.eu.org>
2024-12-08 15:44:37 +0100

    test: update

--- test/test.ml
+++ test/test.ml
@@ -1,25 +1,28 @@
+open Alcotest
 open Murmur3.Murmur_cstruct
+
+let cstr = testable Cstruct.hexdump_pp Cstruct.equal
 
 let runtest () =
   let empty = Cstruct.create 0 in
   let result = Cstruct.of_string "\000\000\000\000" in
   murmur_x86_32 result empty 0l ;
   let expected = Cstruct.of_string "\000\000\000\000" in
-  assert (Cstruct.equal result expected) ;
+  check cstr "empty" expected result ;
 
   let expected = Cstruct.of_string "\xb7\x28\x4e\x51" in
   murmur_x86_32 result empty 1l ;
-  assert (Cstruct.equal result expected) ;
+  check cstr "v1" expected result ;
 
   let expected = Cstruct.of_string "\x39\x6f\xf1\x81" in
   murmur_x86_32 result empty 0xffffffffl ;
-  assert (Cstruct.equal result expected)
+  check cstr "v2" expected result
 
 let vectors = [
   "Vectors", `Quick, runtest ;
 ]
 
 let () =
-  Alcotest.run "Murmur" [
+  run "Murmur" [
     "vectors", vectors ;
   ]
